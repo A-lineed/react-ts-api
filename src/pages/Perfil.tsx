75941af472f16cd2f9fe3dfad61132de761efe7f
A-lineed <alineedvania2018@gmail.com>
2022-11-03 23:50:40 -0300

    Estilizando a página de perfil

--- src/pages/Perfil.tsx
+++ src/pages/Perfil.tsx
@@ -1,11 +1,32 @@
 import Layout from "../components/Layout/Layout";
 
-
-
 const Perfil = () => {
-    return (
-     <Layout> Perfil </Layout>
-    );
-}
+  return (
+    <Layout>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">Tecnologias</th>
+            <th scope="col">Tipo</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>React</td>
+            <td>Frontend</td>
+          </tr>
+          <tr>
+            <td>Angular</td>
+            <td>Frontend</td>
+          </tr>
+          <tr>
+            <td>Node</td>
+            <td>Backend</td>
+          </tr>
+        </tbody>
+      </table>
+    </Layout>
+  );
+};
 
 export default Perfil;
