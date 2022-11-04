dbfa0f2638a4f43d6360a1171dc5ae6c9321588c
A-lineed <alineedvania2018@gmail.com>
2022-11-03 23:40:03 -0300

    Estilizando página inicial

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,27 @@ import Layout from "../components/Layout/Layout";
 import Menu from "../components/Menu/Menu";
 
 const Home = () => {
-  return <Layout>Home Page</Layout>;
+  return (
+    <Layout>
+      <div className=" col-4 m-auto">
+        <h1>Login</h1>
+        <div className="mb-3">
+          <label htmlFor="gitUser" className="form-label">
+            Usuário do GitHub
+          </label>
+          <input
+            className="form-control"
+            id="gitUser"
+            aria-describedby="userlHelp"
+          />
+          <div id="userHelp" className="form-text">
+            Informe o usuário do GitHub
+          </div>
+        </div>
+        <button type="button" className="btn btn-outline-success">Entrar</button>
+      </div>
+    </Layout>
+  );
 };
 
 export default Home;
